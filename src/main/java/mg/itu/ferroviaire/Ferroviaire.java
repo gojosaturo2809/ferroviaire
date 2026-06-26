f4aff12ca16c3f020d9825a3c54479f629031ff7
Hasiman <hasimanjaka2809@gmail.com>
2026-06-26 21:25:05 +0300

    first commit

---
+++ src/main/java/mg/itu/ferroviaire/Ferroviaire.java
@@ -0,0 +1,12 @@
+package mg.itu.ferroviaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ferroviaire {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ferroviaire.class, args);
+	}
+}
